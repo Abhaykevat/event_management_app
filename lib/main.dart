c80ec57fd4c2d51808445e945a874343108fb837
Abhaykevat <abhaykevat84@gmail.com>
2024-07-29 00:44:55 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:event_management_app/saved_data.dart';
+// import 'package:event_management_app/views/create_event_page.dart';
+// import 'package:event_management_app/views/homepage.dart';
+import 'package:event_management_app/views/login.dart';
+// import 'package:event_management_app/views/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+   await SavedData.init();
+   HttpOverrides.global=MyHttpOverrides();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ).copyWith(textTheme: GoogleFonts.interTextTheme()),
+      home:LoginPage(),
+    );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
